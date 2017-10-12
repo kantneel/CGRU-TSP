a0048bd1bd7c185d7daab9fb9595fd4608d102e5
Neel Kant <kantneel@gmail.com>
2017-10-12 11:33:17 -0700

    more sharpening

--- modeling/model_utils.py
+++ modeling/model_utils.py
@@ -1,24 +1,30 @@
 import tensorflow as tf 
-import numpy as tf 
+import numpy as np 
 
-def valid_loss(matrix, rowcol_coef, frob_coef, v):
-	frob_squared = tf.reduce_sum(tf.square(matrix))
+def valid_loss(matrix, rowcol_coef, equal_coef, v):
 
 	col_sums = tf.reduce_sum(matrix, axis=0)
 	row_sums = tf.reduce_sum(matrix, axis=1)
 	ones = tf.ones(v)
 
-	rowcol_loss = rowcol_coef * (tf.nn.l2_loss(col_sums - ones) + 
-								  tf.nn.l2_loss(row_sums - ones))
-	frob_loss = frob_coef * tf.square(v - frob_squared)
+	subd = tf.abs(matrix - 1 / v)
 
-	return rowcol_loss
+	rowcol_loss = rowcol_coef * (tf.nn.l2_loss(row_sums - ones) + tf.nn.l2_loss(col_sums - ones))
+	equal_loss = - equal_coef * tf.norm(tf.square(tf.square(subd)))
+
+	return rowcol_loss + equal_loss
 
 def cycle_loss(res, label, cycle_coef):
 	return cycle_coef * tf.nn.l2_loss(res - label)
 
-def power_and_norm(x):
-    for i in range(6):
-        x = tf.pow(x, 6)
-        x /= tf.reduce_sum(x, axis=1)
-    return x 
+
+def power_and_norm(x, v):
+	x_r = x + tf.zeros([v, v])
+	x_c = tf.transpose(x + tf.zeros([v, v]))
+	for i in range(6):
+		x_r = tf.pow(x_r, 2)
+		x_r /= tf.reduce_sum(x_r, axis=0)
+	for i in range(6):
+		x_c = tf.pow(x_c, 2)
+		x_c /= tf.reduce_sum(x_c, axis=0)
+	return 0.5 * x_r + 0.5 * tf.transpose(x_c)
--- modeling/test2.py
+++ modeling/test2.py
@@ -1,9 +1,9 @@
 import numpy as np 
-import pandas as pd
 import tensorflow as tf 
 
 from neural_gpu import *
 from graph_utils import *
+from model_utils import *
 
 def conv_linear(arg, kw, kh, nin, nout, stride, do_bias, prefix, bias_start):
 	# Notes 
@@ -22,34 +22,15 @@ def conv_linear(arg, kw, kh, nin, nout, stride, do_bias, prefix, bias_start):
 
 def conv_gru(inpts, kw, kh, nin, nout, prefix):
 	"""Convolutional GRU."""
-
-		#return layer_norm(res, nmaps, prefix + "/" + suffix)
 	reset = tf.sigmoid(conv_linear(inpts, kw, kh, nin, nin, [1, 1], True, prefix + "/r", 0.1))
 	gate = tf.sigmoid(conv_linear(inpts, kw, kh, nin, nin, [1, 1], True, prefix + "/g", 0.1))
 	cand = tf.tanh(conv_linear(inpts * reset, kw, kh, nin, nin, [1, 1], True, prefix + "/c", 0.0))
-	'''
-	if cutoff == 1.2:
-		reset = sigmoid_cutoff_12(conv_lin(inpts , "r"))
-		gate = sigmoid_cutoff_12(conv_lin(inpts, "g"))
-	elif cutoff > 10:
-		reset = sigmoid_hard(conv_lin(inpts, "r"))
-		gate = sigmoid_hard(conv_lin(inpts, "g"))
-	else:
-		reset = sigmoid_cutoff(conv_lin(inpts, "r"), cutoff)
-		gate = sigmoid_cutoff(conv_lin(inpts, "g"), cutoff)
-
-	if cutoff > 10:
-		candidate = tanh_hard(conv_lin(inpts * reset, "c", 0.0))
-	else:
-		# candidate = tanh_cutoff(conv_lin(inpts + [reset * mem], "c", 0.0), cutoff)
-		candidate = tf.tanh(conv_lin(inpts * reset, "c", 0.0))
-	'''
 	return gate * inpts + (1 - gate) * cand
 
 train_data = np.loadtxt('../graph_data/10v_data_1_1.csv', delimiter=',').reshape((4096, 10, 10))
 train_labels = np.loadtxt('../graph_data/10v_labels_1_1.csv', delimiter=',').reshape((4096, 10, 10))
 
-b_size = 4
+b_size = 10
 g_size = 10
 num_data = 4096
 
@@ -62,27 +43,23 @@ start = tf.concat((x_image, tf.random_normal((b_size, g_size, g_size, 19))), axi
 
 img_1 = conv_gru(start, 5, 5, 20, 20, "cgru1")
 img_2 = conv_gru(img_1, 5, 5, 20, 20, "cgru2")
-img_3 = conv_gru(img_2, 5, 5, 20, 20, "cgru3")
-img_4 = conv_gru(img_3, 3, 3, 20, 20, "cgru4")
-img_5 = conv_gru(img_4, 3, 3, 20, 20, "cgru5")
-img_6 = conv_gru(img_5, 2, 2, 20, 20, "cgru6")
+img_3 = conv_gru(img_2, 3, 3, 20, 20, "cgru3")
+img_4 = conv_gru(img_3, 2, 2, 20, 20, "cgru4")
 
-
-result = tf.reshape(img_6[:, :, :, 0], batch_shape)
-results = [tf.nn.softmax(tf.reshape(result[i], [g_size, g_size])) for i in range(b_size)]
+result = tf.reshape(img_4[:, :, :, 0], batch_shape)
+results = [power_and_norm(tf.reshape(result[i], [g_size, g_size]), g_size) for i in range(b_size)]
 t_results = [results[i] + tf.transpose(results[i]) for i in range(b_size)]
 labels = [tf.reshape(y[i], [g_size, g_size]) for i in range(b_size)]
 
-v_loss = np.sum([valid_loss(results[i], 0.1, 1) for i in range(b_size)])
-c_loss = np.sum([cycle_loss(t_results[i], labels[i], 1) for i in range(b_size)])
+v_loss = np.sum([valid_loss(results[i], 0, 1, g_size) for i in range(b_size)]) / b_size
+c_loss = np.sum([cycle_loss(t_results[i], labels[i], 1) for i in range(b_size)]) / b_size
 
-train_step_v = tf.train.AdamOptimizer(1e-3).minimize(v_loss)
-train_step_c = tf.train.AdamOptimizer(1e-3).minimize(c_loss)
-
-
+train_step_v = tf.train.AdamOptimizer(1e-2).minimize(v_loss)
+train_step_c = tf.train.AdamOptimizer(1e-7).minimize(c_loss)
 
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
+	avg = 0
 	for i in range(10000):
 		if (b_size * i) % num_data > (b_size * (i + 1)) % num_data:
 			continue
@@ -92,8 +69,11 @@ with tf.Session() as sess:
 		res, v, vs = sess.run([results, v_loss, train_step_v], feed_dict={x : batch_data, y : batch_labels})
 		c, vc = sess.run([c_loss, train_step_c], feed_dict={x : batch_data, y : batch_labels})
 
+		avg += v
 		if i % 20 == 0:
-			print(i, v, c)
+			avg /= 20
+			print(i, avg, c)
+			avg = 0
 			if i % 200 == 0:
-				print(res)
+				print(np.round(2 * res[0]))
 
