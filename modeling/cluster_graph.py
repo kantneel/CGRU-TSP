d706e9f8487217002359d572fbb21b0980a1ccba
Neel Kant <kantneel@gmail.com>
2017-10-20 00:57:55 -0700

    cluster graph code

---
+++ modeling/cluster_graph.py
@@ -0,0 +1,32 @@
+import numpy as np 
+import math
+
+def cluster_graph(vertices, max_coord, n_clusters, max_pop, p_rad, p_arc):
+	assignments = []
+	p_coords = []
+	r_low = p_rad * max_coord / 2
+	for i in range(n_clusters):
+		for _ in range(max_pop):
+			assignments.append(i)
+
+	assign = np.array(assignments)
+	np.random.shuffle(assign)
+	final_assign = assign[:vertices]
+
+
+	for i in range(vertices):
+		r = np.random.uniform(low=r_low, high=max_coord / 2)
+		theta = 360 * (final_assign[i] + np.random.uniform(low=0, high=p_arc))  / n_clusters
+		p_coords.append([r, theta])
+
+	offset = np.random.uniform(low=0, high=1) * 360 / n_clusters
+	p_coords = [[c[0], (c[1] + offset) % 360] for c in p_coords]
+	eu_coords = [polar_to_euclid(c) for c in p_coords]
+
+
+def polar_to_euclid(polar):
+	r = polar[0]
+	theta = math.radians(polar[1])
+
+	return [r * math.cos(theta), r * math.sin(theta)]
+
