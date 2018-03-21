c0550ccbc317aa555625a989f45110da58211edf
Neel Kant <kantneel@berkeley.edu>
2018-03-21 06:59:14 -0700

    linting and minor modifications to graph_utils

--- modeling/data_gen.py
+++ modeling/data_gen.py
@@ -7,79 +7,81 @@ from graph_utils import *
 from graph import Graph
 
 def gen_and_save_data(num_vertices, border_vertices, max_coord, shard_size, metric, symmetric, relative, approx, prefix):
-	# generate num_shards files of graphs, each having shard_size graphs. 
-	# graphs will be uniform in characteristics of num_vertices, max_coord, metric
-	label_func = eu_shortest_cycle if approx else ex_shortest_cycle
-	gen_func = pc_graph if metric else non_pc_graph
+    # generate num_shards files of graphs, each having shard_size graphs. 
+    # graphs will be uniform in characteristics of num_vertices, max_coord, metric
+    label_func = eu_shortest_cycle if approx else ex_shortest_cycle
+    gen_func = pc_graph if metric else non_pc_graph
 
-	data_array = np.zeros((shard_size, border_vertices ** 2))
-	label_array = np.zeros((shard_size, border_vertices ** 2))
-	for j in range(shard_size):
-		if j % 100 == 0:
-			print(j)
-		g = gen_func(num_vertices, max_coord, relative) + 10 * max_coord * np.eye(num_vertices)
-		zero_g = -1 * np.ones((border_vertices, border_vertices))
-		zero_g[:num_vertices, :num_vertices] = g 
-		data_array[j] = zero_g.reshape(1, border_vertices ** 2)
+    data_array = np.zeros((shard_size, border_vertices ** 2))
+    label_array = np.zeros((shard_size, border_vertices ** 2))
+    for j in range(shard_size):
+        if j % 100 == 0:
+            print(j)
+        g = gen_func(num_vertices, max_coord, relative) + 10 * max_coord * np.eye(num_vertices)
+        zero_g = -1 * np.ones((border_vertices, border_vertices))
+        zero_g[:num_vertices, :num_vertices] = g 
+        data_array[j] = zero_g.reshape(1, border_vertices ** 2)
 
-		l = label_func(g, True, symmetric)
-		zero_l = (1 + (int(symmetric))) * np.eye(border_vertices)
-		zero_l[:num_vertices, :num_vertices] = l
-		label_array[j] = zero_l.reshape(1, border_vertices ** 2)
-	print("HELLSDLFKJSDLFIJ")
-	np.savetxt(prefix + "_data.csv", data_array, delimiter=',')
-	np.savetxt(prefix + "_labels.csv", label_array, delimiter=',')
+        l = label_func(g, True, symmetric)
+        zero_l = (1 + (int(symmetric))) * np.eye(border_vertices)
+        zero_l[:num_vertices, :num_vertices] = l
+        label_array[j] = zero_l.reshape(1, border_vertices ** 2)
+    
+    print("Completed Data Generation")
+    np.savetxt(prefix + "_data.csv", data_array, delimiter=',')
+    np.savetxt(prefix + "_labels.csv", label_array, delimiter=',')
 
 def gen_curriculum(num_vertices, border_vertices, max_coord, shard_size, approx, 
-					n_clusters, max_pop, p_rad, p_arc, prefix):
+                    n_clusters, max_pop, p_rad, p_arc, prefix):
 	
-	label_func = eu_shortest_cycle if approx else ex_shortest_cycle
+    label_func = eu_shortest_cycle if approx else ex_shortest_cycle
 
-	data_array = np.zeros((shard_size, border_vertices ** 2))
-	label_array = np.zeros((shard_size, border_vertices ** 2))
-	for j in range(shard_size):
-		g = cluster_graph(num_vertices, max_coord, n_clusters, max_pop, p_rad, p_arc) + 10 * max_coord * np.eye(num_vertices)
-		zero_g = -max_coord / 15 * np.ones((border_vertices, border_vertices))
-		zero_g[:num_vertices, :num_vertices] = g 
-		data_array[j] = zero_g.reshape(1, border_vertices ** 2)
+    data_array = np.zeros((shard_size, border_vertices ** 2))
+    label_array = np.zeros((shard_size, border_vertices ** 2))
+    for j in range(shard_size):
+        g = cluster_graph(num_vertices, max_coord, n_clusters, max_pop, p_rad, p_arc) + 10 * max_coord * np.eye(num_vertices)
+        zero_g = -max_coord / 15 * np.ones((border_vertices, border_vertices))
+        zero_g[:num_vertices, :num_vertices] = g 
+        data_array[j] = zero_g.reshape(1, border_vertices ** 2)
 
-		l = label_func(g, True, True)
-		zero_l = 2 * np.eye(border_vertices)
-		zero_l[:num_vertices, :num_vertices] = l
-		label_array[j] = zero_l.reshape(1, border_vertices ** 2)
-	print("HELLSDLFKJSDLFIJ")
-	np.savetxt(prefix + "_data.csv", data_array, delimiter=',')
-	np.savetxt(prefix + "_labels.csv", label_array, delimiter=',')
-	
-
+        l = label_func(g, True, True)
+        zero_l = 2 * np.eye(border_vertices)
+        zero_l[:num_vertices, :num_vertices] = l
+        label_array[j] = zero_l.reshape(1, border_vertices ** 2)
+    
+    print("Completed Curriculum Generation")
+    np.savetxt(prefix + "_data.csv", data_array, delimiter=',')
+    np.savetxt(prefix + "_labels.csv", label_array, delimiter=',')
+ 
 def gen_sharpening_data(num_vertices, num_shards, shard_size, prefix):
-	for i in range(1, num_shards + 1):
-		data_array = np.zeros((shard_size, num_vertices ** 2))
-		label_array = np.zeros((shard_size, num_vertices ** 2))
-		for j in range(shard_size):
-			num = np.random.randint(1, high=4)
-			start = np.random.uniform(-num, num, (num_vertices, num_vertices))
-			data_array[j] = start.reshape(1, num_vertices ** 2)
+    for i in range(1, num_shards + 1):
+        data_array = np.zeros((shard_size, num_vertices ** 2))
+        label_array = np.zeros((shard_size, num_vertices ** 2))
+        for j in range(shard_size):
+            num = np.random.randint(1, high=4)
+            start = np.random.uniform(-num, num, (num_vertices, num_vertices))
+            data_array[j] = start.reshape(1, num_vertices ** 2)
 
-			softmaxed = np.exp(start) / np.sum(np.exp(start), axis=1)
-			maxes = np.argmax(softmaxed, axis=1)
-			new_array = np.zeros((num_vertices, num_vertices))
-			for k in range(num_vertices):
-				new_array[k, maxes[k]] = 1
-			label_array[j] = new_array.reshape(1, num_vertices ** 2)
-		np.savetxt(prefix + "_data_%s_%s.csv" % (i, num_shards), data_array, delimiter=',')
-		np.savetxt(prefix + "_labels_%s_%s.csv" % (i, num_shards), label_array, delimiter=',')
+            softmaxed = np.exp(start) / np.sum(np.exp(start), axis=1)
+            maxes = np.argmax(softmaxed, axis=1)
+            new_array = np.zeros((num_vertices, num_vertices))
+            for k in range(num_vertices):
+                    new_array[k, maxes[k]] = 1
+            label_array[j] = new_array.reshape(1, num_vertices ** 2)
+        np.savetxt(prefix + "_data_%s_%s.csv" % (i, num_shards), data_array, delimiter=',')
+        np.savetxt(prefix + "_labels_%s_%s.csv" % (i, num_shards), label_array, delimiter=',')
 
 
 #gen_and_save_data(5, 10, 100, 50008, True, True, True, False, "../graph_data/5v")
 #gen_sharpening_data(10, 1, 40000, "../sharpening/10v")
 
-gen_curriculum(15, 15, 1, 10000, True, 5, 5, 0.99, 0.4, "../graph_data/15_0")
-gen_curriculum(15, 15, 1, 10000, True, 5, 5, 0.95, 0.5, "../graph_data/15_1")
-gen_curriculum(15, 15, 1, 10000, True, 5, 5, 0.8, 0.2, "../graph_data/15_2")
-gen_curriculum(15, 15, 1, 10000, True, 5, 5, 0.5, 0.2, "../graph_data/15_3")
-gen_curriculum(15, 15, 1, 10000, True, 5, 5, 0.6, 0.4, "../graph_data/15_4")
-gen_curriculum(15, 15, 1, 10000, True, 5, 5, 0.3, 0.6, "../graph_data/15_5")
-gen_and_save_data(15, 15, 1, 10000, True, True, True, True, "../graph_data/15_6")
+if __name__ == "__main__":
+    gen_curriculum(15, 15, 1, 10000, True, 5, 5, 0.99, 0.4, "../graph_data/15_0")
+    gen_curriculum(15, 15, 1, 10000, True, 5, 5, 0.95, 0.5, "../graph_data/15_1")
+    gen_curriculum(15, 15, 1, 10000, True, 5, 5, 0.8, 0.2, "../graph_data/15_2")
+    gen_curriculum(15, 15, 1, 10000, True, 5, 5, 0.5, 0.2, "../graph_data/15_3")
+    gen_curriculum(15, 15, 1, 10000, True, 5, 5, 0.6, 0.4, "../graph_data/15_4")
+    gen_curriculum(15, 15, 1, 10000, True, 5, 5, 0.3, 0.6, "../graph_data/15_5")
+    gen_and_save_data(15, 15, 1, 10000, True, True, True, True, "../graph_data/15_6")
 
 
--- modeling/graph_utils.py
+++ modeling/graph_utils.py
@@ -7,188 +7,186 @@ from graph import Graph
 import tensorflow as tf
 
 def is_subgraph(graph, edges):
-	return set(edges) <= set(graph.edge_dict.keys())
+    return set(edges) <= set(graph.edge_dict.keys())
 
 def is_tree(num_vertices, edges):
-	if len(edges) != 2 * (num_vertices - 1) and len(edges) != num_vertices - 1:
-		print("incorrect number of edges")
-		return False
-	matrix = np.zeros((num_vertices, num_vertices))
-	for i in range(num_vertices):
-		for j in range(num_vertices):
-			if (i, j) in edges:
-				matrix[i, j] = 1
-				matrix[j, i] = 1
+    if len(edges) != 2 * (num_vertices - 1) and len(edges) != num_vertices - 1:
+        print("incorrect number of edges")
+        return False
+    matrix = np.zeros((num_vertices, num_vertices))
+    for i in range(num_vertices):
+        for j in range(num_vertices):
+            if (i, j) in edges:
+                matrix[i, j] = 1
+                matrix[j, i] = 1
 
-	g = Graph(matrix)
+    g = Graph(matrix)
 
-	if len(g.get_connected_vertices(0)) != num_vertices:
-		print("nope, not connected")
-		return False
-	return True
+    if len(g.get_connected_vertices(0)) != num_vertices:
+        print("nope, not connected")
+        return False
+    return True
 
 def is_cycle(matrix):
-	# matrix is V x V np array. Check that every vertex is represented exactly once. 
-	v = matrix.shape[0]
-	zero = np.ones(v) == np.sum(matrix, axis=0)
-	one = np.ones(v) == np.sum(matrix, axis=1)
-	return zero and one
+    # matrix is V x V np array. Check that every vertex is represented exactly once. 
+    v = matrix.shape[0]
+    zero = np.ones(v) == np.sum(matrix, axis=0)
+    one = np.ones(v) == np.sum(matrix, axis=1)
+    return zero and one
 
 def distance(p1, p2):
-	# p1, p2 are 2D tuples. Calculate euclidean distance
-	return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+    # p1, p2 are 2D tuples. Calculate euclidean distance
+    return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
 def cluster_graph(vertices, max_coord, n_clusters, max_pop, p_rad, p_arc):
-	assignments = []
-	p_coords = []
-	r_low = p_rad * max_coord / 2
-	for i in range(n_clusters):
-		for _ in range(max_pop):
-			assignments.append(i)
+    assignments = []
+    p_coords = []
+    r_low = p_rad * max_coord / 2
+    for i in range(n_clusters):
+        for _ in range(max_pop):
+            assignments.append(i)
 
-	assign = np.array(assignments)
-	np.random.shuffle(assign)
-	final_assign = assign[:vertices]
+    assign = np.array(assignments)
+    np.random.shuffle(assign)
+    final_assign = assign[:vertices]
 
+    for i in range(vertices):
+        r = np.random.uniform(low=r_low, high=max_coord / 2)
+        theta = 360 * (final_assign[i] + np.random.uniform(low=0, high=p_arc))  / n_clusters
+        p_coords.append([r, theta])
 
-	for i in range(vertices):
-		r = np.random.uniform(low=r_low, high=max_coord / 2)
-		theta = 360 * (final_assign[i] + np.random.uniform(low=0, high=p_arc))  / n_clusters
-		p_coords.append([r, theta])
+    offset = np.random.uniform(low=0, high=1) * 360 / n_clusters
+    p_coords = [[c[0], (c[1] + offset) % 360] for c in p_coords]
+    eu_coords = [polar_to_euclid(c) for c in p_coords]
+    eu_coords = [[e[0] + max_coord / 2, e[1] + max_coord / 2] for e in eu_coords]
+    eu_coords.sort(key=lambda x: distance((0, 0), x))
 
-	offset = np.random.uniform(low=0, high=1) * 360 / n_clusters
-	p_coords = [[c[0], (c[1] + offset) % 360] for c in p_coords]
-	eu_coords = [polar_to_euclid(c) for c in p_coords]
-	eu_coords = [[e[0] + max_coord / 2, e[1] + max_coord / 2] for e in eu_coords]
-	eu_coords.sort(key=lambda x: distance((0, 0), x))
+    edges = np.zeros((vertices, vertices))
+    for i in range(vertices):
+        for j in range(vertices):
+            if i == j:
+                continue
+            edges[i, j] = distance(eu_coords[i], eu_coords[j])
 
-	edges = np.zeros((vertices, vertices))
-	for i in range(vertices):
-		for j in range(vertices):
-			if i == j:
-				continue
-			edges[i, j] = distance(eu_coords[i], eu_coords[j])
-
-	return edges
-
+    return edges
 
 def polar_to_euclid(polar):
-	r = polar[0]
-	theta = math.radians(polar[1])
+    r = polar[0]
+    theta = math.radians(polar[1])
 
-	return [r * math.cos(theta), r * math.sin(theta)]
+    return [r * math.cos(theta), r * math.sin(theta)]
 
 def pc_graph(num_vertices, max_coord, relative):
-	# planar_connected_graph
-	# generate points in 2D space, in first quadrant max_coord x max_coord
-	# get distances between points to generate 2D planar graph. 
-	# returns: edges -> a symmetric matrix of edge lengths
-	coords = []
-	for i in range(num_vertices):
-		coords.append(max_coord * np.random.rand(2))
+    # planar_connected_graph
+    # generate points in 2D space, in first quadrant max_coord x max_coord
+    # get distances between points to generate 2D planar graph. 
+    # returns: edges -> a symmetric matrix of edge lengths
+    coords = []
+    for i in range(num_vertices):
+        coords.append(max_coord * np.random.rand(2))
 
-	if relative:
-		coords.sort(key=lambda x: distance((0,0), x))
+    if relative:
+        coords.sort(key=lambda x: distance((0,0), x))
 
-	edges = np.zeros((num_vertices, num_vertices))
-	for i in range(num_vertices):
-		for j in range(num_vertices):
-			if i == j:
-				continue
-			edges[i, j] = distance(coords[i], coords[j])
+    edges = np.zeros((num_vertices, num_vertices))
+    for i in range(num_vertices):
+        for j in range(num_vertices):
+            if i == j:
+                continue
+            edges[i, j] = distance(coords[i], coords[j])
 
-	return edges
+    return edges
 
 def non_pc_graph(num_vertices, max_dist, relative):
-	edges = np.zeros((num_vertices, num_vertices))
-	for i in range(num_vertices):
-		for j in range(i + 1, num_vertices):
-			edge_val = max_dist * np.random.rand()
-			edges[i, j] = edge_val
-			edges[j, i] = edge_val
-	return edges
+    edges = np.zeros((num_vertices, num_vertices))
+    for i in range(num_vertices):
+        for j in range(i + 1, num_vertices):
+            edge_val = max_dist * np.random.rand()
+            edges[i, j] = edge_val
+            edges[j, i] = edge_val
+    return edges
 
 def eu_shortest_cycle(graph_matrix, in_matrix=True, with_transpose=True):
-	# takes a edge matrix as given by planar_connected_graph
-	# checks (n-1)! different paths by setting vertex n to be the last vertex seen before the first one
-	# i.e. n = 5 -> permute(1-4), 5 -> 32415 
+    # takes a edge matrix as given by planar_connected_graph
+    # checks (n-1)! different paths by setting vertex n to be the last vertex seen before the first one
+    # i.e. n = 5 -> permute(1-4), 5 -> 32415 
 
-	# algorithm credits: http://gregorulm.com/finding-an-eulerian-path/
+    # algorithm credits: http://gregorulm.com/finding-an-eulerian-path/
 
-	graph = Graph(graph_matrix)
-	mst = graph.get_mst()
+    graph = Graph(graph_matrix)
+    mst = graph.get_mst()
 
-	def freqencies():
-		my_list = [x for (x, y) in mst]
-		result = [0 for i in range(max(my_list) + 1)]
-		for i in my_list:
-			result[i] += 1
-		return result
-		 
-	def find_node(tour):
-		for i in tour:
-			if freq[i] != 0:
-				return i
-		return -1
-	 
-	def helper(tour, next):
-		find_path(tour, next)
-		u = find_node(tour)
-		while sum(freq) != 0:     
-			sub = find_path([], u)
-			tour = tour[:tour.index(u)] + sub + tour[tour.index(u) + 1:]  
-			u = find_node(tour)
-		return tour
-				  
-	def find_path(tour, next):
-		for (x, y) in mst:
-			if x == next:
-				current = mst.pop(mst.index((x,y)))
-				mst.pop(mst.index((current[1], current[0])))
-				tour.append(current[0])
-				freq[current[0]] -= 1
-				freq[current[1]] -= 1
-				return find_path(tour, current[1])
-		tour.append(next)
-		return tour             
-			  
-	freq = freqencies()  
-	cycle = helper([], mst[0][0])
-	if in_matrix:
-		return cycle_matrix(cycle, with_transpose)
-	return cycle
+    def freqencies():
+        my_list = [x for (x, y) in mst]
+        result = [0 for i in range(max(my_list) + 1)]
+        for i in my_list:
+            result[i] += 1
+        return result
+             
+    def find_node(tour):
+        for i in tour:
+            if freq[i] != 0:
+                return i
+        return -1
+     
+    def helper(tour, next):
+        find_path(tour, next)
+        u = find_node(tour)
+        while sum(freq) != 0:     
+            sub = find_path([], u)
+            tour = tour[:tour.index(u)] + sub + tour[tour.index(u) + 1:]  
+            u = find_node(tour)
+        return tour
+                              
+    def find_path(tour, next):
+        for (x, y) in mst:
+            if x == next:
+                current = mst.pop(mst.index((x,y)))
+                mst.pop(mst.index((current[1], current[0])))
+                tour.append(current[0])
+                freq[current[0]] -= 1
+                freq[current[1]] -= 1
+                return find_path(tour, current[1])
+        tour.append(next)
+        return tour             
+                      
+    freq = freqencies()  
+    cycle = helper([], mst[0][0])
+    if in_matrix:
+        return cycle_matrix(cycle, with_transpose)
+    return cycle
 
 def ex_shortest_cycle(graph_matrix, in_matrix=True, with_transpose=True):
-	num_vertices = graph_matrix.shape[0]
-	perm_list = list(itertools.permutations(np.arange(num_vertices - 1).tolist()))
+    num_vertices = graph_matrix.shape[0]
+    perm_list = list(itertools.permutations(np.arange(num_vertices - 1).tolist()))
 
-	cycle_lengths = []
-	for i in range(len(perm_list)):
-		verts = list(perm_list[i])
-		verts.append(num_vertices - 1)
-		cycle_lengths.append(cycle_cost(graph_matrix, verts)) 
+    cycle_lengths = []
+    for i in range(len(perm_list)):
+        verts = list(perm_list[i])
+        verts.append(num_vertices - 1)
+        cycle_lengths.append(cycle_cost(graph_matrix, verts)) 
 
-	ind = cycle_lengths.index(min(cycle_lengths))
-	cycle = list(perm_list[ind]) + [num_vertices - 1]
-	if in_matrix:
-		return cycle_matrix(cycle, with_transpose)
-	return cycle
+    ind = cycle_lengths.index(min(cycle_lengths))
+    cycle = list(perm_list[ind]) + [num_vertices - 1]
+    if in_matrix:
+        return cycle_matrix(cycle, with_transpose)
+    return cycle
 
 def cycle_cost(graph_matrix, cycle):
-	num_vertices = graph_matrix.shape[0]
-	cycle.append(cycle[0])
-	total = 0
-	for j in range(num_vertices):
-		total += graph_matrix[cycle[j], cycle[j + 1]]
-	return total 
+    num_vertices = graph_matrix.shape[0]
+    cycle.append(cycle[0])
+    total = 0
+    for j in range(num_vertices):
+        total += graph_matrix[cycle[j], cycle[j + 1]]
+    return total 
 
 def cycle_matrix(cycle, with_transpose=True):
-	v = len(cycle)
-	cycle.append(cycle[0])
-	matrix = np.zeros((v, v))
-	for i in range(v):
-		matrix[cycle[i], cycle[i + 1]] = 1
-	if with_transpose:
-		matrix += matrix.T
-	return matrix
+    v = len(cycle)
+    cycle.append(cycle[0])
+    matrix = np.zeros((v, v))
+    for i in range(v):
+        matrix[cycle[i], cycle[i + 1]] = 1
+    if with_transpose:
+        matrix += matrix.T
+    return matrix
 
